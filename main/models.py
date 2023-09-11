c027462962f1275988c0175ea0185a5339ac2c9a
raisahzr <raisa.hannaliya@gmail.com>
2023-09-11 10:34:17 +0700

    (+)unit testing check stock

--- main/models.py
+++ main/models.py
@@ -7,3 +7,9 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
     description = models.TextField()
+
+    def check_stock(self):
+        if self.amount > 0:
+            return f"Stock of {self.name} is {self.amount}!"
+        else:
+            return f"Stock of {self.name} is not available."
--- main/tests.py
+++ main/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.test import TestCase, Client
+from main.models import Product
 
 # Create your tests here.
 class mainTest(TestCase):
@@ -10,3 +11,17 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+class ProductTestCase(TestCase):
+    def setUp(self):
+        Product.objects.create(name="ice americano", amount="0")
+        Product.objects.create(name="caffe latte", amount="10")
+
+    def test_products_has_stock(self):
+        # check if product has stocks
+        caffe_latte = Product.objects.get(name="caffe latte")
+        self.assertEqual(caffe_latte.check_stock(), 'Stock of caffe latte is 10!')
+
+    def test_products_has_no_stock(self):
+        ice_americano = Product.objects.get(name="ice americano")
+        self.assertEqual(ice_americano.check_stock(), 'Stock of ice americano is not available.')
