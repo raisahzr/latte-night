332a2d126ec57e60255d435c850eaf77e84fe6f0
raisahzr <raisa.hannaliya@gmail.com>
2023-09-27 09:06:40 +0700

    Tugas 4

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Item(models.Model):
@@ -8,6 +9,7 @@ class Item(models.Model):
     description = models.TextField()
     price = models.IntegerField()
     category = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def check_stock(self):
         if self.amount > 0:
